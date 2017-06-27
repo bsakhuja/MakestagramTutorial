c679494d99888f829ed1db7c9a743fa89ef81981
bsakhuja <briansakhuja@gmail.com>
2017-06-27 16:47:23 -0700

    improved upload code and added post class

---
+++ Makestagram/Models/Post.swift
@@ -0,0 +1,33 @@
+//
+//  Post.swift
+//  Makestagram
+//
+//  Created by Brian Sakhuja on 6/27/17.
+//  Copyright © 2017 Brian Sakhuja. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase.FIRDataSnapshot
+
+class Post {
+    var key: String?
+    let imageURL: String
+    let imageHeight: CGFloat
+    let creationDate: Date
+    
+    var dictValue: [String : Any] {
+        let createdAgo = creationDate.timeIntervalSince1970
+        
+        return ["image_url" : imageURL,
+                "image_height" : imageHeight,
+                "created_at" : createdAgo]
+    }
+    
+    init(imageURL: String, imageHeight: CGFloat) {
+        self.imageURL = imageURL
+        self.imageHeight = imageHeight
+        self.creationDate = Date()
+    }
+    
+    
+}
