a439da9f4e909a5535ef298cc2c2b3ef14796154
bsakhuja <briansakhuja@gmail.com>
2017-06-28 10:45:13 -0700

    Completed Step 17

---
+++ Makestagram/Controllers/PostImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  PostImageCell.swift
+//  Makestagram
+//
+//  Created by Brian Sakhuja on 6/28/17.
+//  Copyright © 2017 Brian Sakhuja. All rights reserved.
+//
+
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    // MARK: - Subviews
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    // MARK: - Cell Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
